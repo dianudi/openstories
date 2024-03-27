5782dc397a64174eb1b9f40764805a3a542b9b08
dianudi <144350056+dianudi@users.noreply.github.com>
2024-03-27 21:06:46 +0700

    modify ui card

---
+++ resources/js/Pages/Auth/ForgotPassword.jsx
@@ -0,0 +1,62 @@
+import GuestLayout from "@/Layouts/GuestLayout";
+import InputError from "@/Components/InputError";
+import PrimaryButton from "@/Components/PrimaryButton";
+import TextInput from "@/Components/TextInput";
+import { Head, Link, useForm } from "@inertiajs/react";
+
+export default function ForgotPassword({ status }) {
+    const { data, setData, post, processing, errors } = useForm({
+        email: "",
+    });
+
+    const submit = (e) => {
+        e.preventDefault();
+
+        post(route("password.email"));
+    };
+
+    return (
+        <GuestLayout>
+            <Head title="Forgot Password" />
+            <h1 className="text-center text-3xl mb-2">Forgot Password</h1>
+            <div className="mb-4 text-sm text-gray-600">
+                Forgot your password? No problem. Just let us know your email
+                address and we will email you a password reset link that will
+                allow you to choose a new one.
+            </div>
+
+            {status && (
+                <div className="mb-4 font-medium text-sm text-green-600">
+                    {status}
+                </div>
+            )}
+
+            <form onSubmit={submit}>
+                <TextInput
+                    id="email"
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    value={data.email}
+                    className="mt-1 block w-full"
+                    isFocused={true}
+                    onChange={(e) => setData("email", e.target.value)}
+                />
+
+                <InputError message={errors.email} className="mt-2" />
+
+                <div className="flex items-center justify-end mt-4">
+                    <PrimaryButton className="ms-4" disabled={processing}>
+                        Email Password Reset Link
+                    </PrimaryButton>
+                </div>
+            </form>
+            <Link
+                href={route("login")}
+                className="text-sm text-gray-600 hover:text-gray-900"
+            >
+                Back to Login
+            </Link>
+        </GuestLayout>
+    );
+}
